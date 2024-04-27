e383c1ac9e804302f65fde1be28fe5d86204d520
Wasi Raza <wasmiester@gmail.com>
2024-04-27 02:02:55 -0700

    Develop (#13)
    * deploy.yml
    
    Signed-off-by: wasmiester <wasmiester@gmail.com>
    
    * update node ver.
    
    Signed-off-by: wasmiester <wasmiester@gmail.com>
    
    * package-lock version fix
    
    * package update
    
    * pacge fix
    
    * yarn update
    
    * yarn update
    
    * react @16
    
    * removed yarn updated pckg
    
    * react update
    
    * package peer dependancy fix
    
    * version fix
    
    * update deploy with --legacy-peer-deps
    
    * ci to false
    
    ---------
    
    Signed-off-by: wasmiester <wasmiester@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import { Component } from "react";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { useState, useEffect, useRef } from "react";
@@ -35,7 +34,6 @@ import {
   DiReact,
   DiNodejsSmall,
   DiMysql,
-  DiJira,
 } from "react-icons/di";
 
 import {
@@ -86,31 +84,31 @@ const rootBgParticles = createRoot(containerBgParticles); // createRoot(containe
 rootBgParticles.render(<BgParticles />);
 //ReactDOM.render(<MapContainer />, document.getElementById("locationMap"));
 
-function useWindowSize() {
-  // Initialize state with undefined width/height so server and client renders match
-  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
-  const [windowSize, setWindowSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  });
-  useEffect(() => {
-    // Handler to call on window resize
-    function handleResize() {
-      // Set window width/height to state
-      setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
-    }
-    // Add event listener
-    window.addEventListener("resize", handleResize);
-    // Call handler right away so state gets updated with initial window size
-    handleResize();
-    // Remove event listener on cleanup
-    return () => window.removeEventListener("resize", handleResize);
-  }, []); // Empty array ensures that effect is only run on mount
-  return windowSize;
-}
+// function useWindowSize() {
+//   // Initialize state with undefined width/height so server and client renders match
+//   // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+//   const [windowSize, setWindowSize] = useState({
+//     width: window.innerWidth,
+//     height: window.innerHeight,
+//   });
+//   useEffect(() => {
+//     // Handler to call on window resize
+//     function handleResize() {
+//       // Set window width/height to state
+//       setWindowSize({
+//         width: window.innerWidth,
+//         height: window.innerHeight,
+//       });
+//     }
+//     // Add event listener
+//     window.addEventListener("resize", handleResize);
+//     // Call handler right away so state gets updated with initial window size
+//     handleResize();
+//     // Remove event listener on cleanup
+//     return () => window.removeEventListener("resize", handleResize);
+//   }, []); // Empty array ensures that effect is only run on mount
+//   return windowSize;
+// }
 
 function useOnScreen(ref, rootMargin = "0px") {
   // State and setter for storing whether element is visible
